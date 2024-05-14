dad65d708e8501952540c00e8b3a0b59b673054a
Aditya Lila <61589143+clawdius@users.noreply.github.com>
2024-05-15 00:41:06 +0700

    Added Error Handling for Auth
    error handling for auth, including duplicate uname and validation for dto

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import {
+  ConflictException,
+  Injectable,
+  InternalServerErrorException,
+} from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from './user.entity';
 import { Repository } from 'typeorm';
@@ -19,6 +23,14 @@ export class AuthService {
       password,
     });
 
-    await this.UsersRepository.insert(user);
+    try {
+      await this.UsersRepository.insert(user);
+    } catch (error) {
+      if (error.code === '23505') { // Duplicate uname
+        throw new ConflictException('Username already exists, my guy.');
+      } else {
+        throw new InternalServerErrorException();
+      }
+    }
   }
 }
