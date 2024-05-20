19da52d88434fb48b8a64f28ed5f7183585e0e4c
Aditya Lila <61589143+clawdius@users.noreply.github.com>
2024-05-20 10:46:01 +0700

    added hash
    inserted hash function with bcrypt

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -7,6 +7,7 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { User } from './user.entity';
 import { Repository } from 'typeorm';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthService {
@@ -18,15 +19,19 @@ export class AuthService {
   async signUp(authCredentialsDto: AuthCredentialsDto): Promise<void> {
     const { username, password } = authCredentialsDto;
 
+    const salt = await bcrypt.genSalt();
+    const hashed = await bcrypt.hash(password, salt);
+
     const user = this.UsersRepository.create({
       username,
-      password,
+      password: hashed,
     });
 
     try {
       await this.UsersRepository.insert(user);
     } catch (error) {
-      if (error.code === '23505') { // Duplicate uname
+      if (error.code === '23505') {
+        // Duplicate uname
         throw new ConflictException('Username already exists, my guy.');
       } else {
         throw new InternalServerErrorException();
